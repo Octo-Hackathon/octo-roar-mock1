de0634d461adaa34db6d5e90c0de4f29aa8ae749
danmontgomery <dan@dmontgomery.net>
2015-03-12 19:43:30 -0400

    Edit and view

--- ManeStreet/public/app/controllers.js
+++ ManeStreet/public/app/controllers.js
@@ -22,6 +22,21 @@ my18fControllers.controller('ManageCtrl', function($scope, $http) {
 /**
  * Controller for user list page, grabs user list
  */
+my18fControllers.controller('ViewCtrl', function($scope, $http) {
+
+    $http.get('/api/contractor/opportunities').success(function(data) {
+
+      $scope.opportunities = data;
+
+    });
+
+    $scope.orderProp = '-id';
+
+});
+
+/**
+ * Controller for user list page, grabs user list
+ */
 my18fControllers.controller('DetailCtrl', function($scope, $http, $routeParams) {
 
     $http.get('/api/government/opportunities/'+$routeParams.oppId).success(function(data) {
@@ -35,13 +50,48 @@ my18fControllers.controller('DetailCtrl', function($scope, $http, $routeParams) 
 /**
  * Controller for user list page, grabs user list
  */
-my18fControllers.controller('EditCtrl', function($scope, $http, $routeParams) {
+my18fControllers.controller('EditCtrl', function($scope, $http, $routeParams, $location) {
 
     $http.get('/api/government/opportunities/'+$routeParams.oppId).success(function(data) {
 
       $scope.opp = data;
 
     });
+
+    $scope.opp = {
+      id: '',
+      title: '',
+      opportunitytype: '',
+      description: '',
+      setaside: '',
+      publishdate: '',
+      responseduedate: '',
+      status: '',
+      buyername: '',
+      placeofperform: '',
+      primaypoc: '',
+      user_id: '',
+      created_at: '',
+      updated_at: ''
+    };
+
+    $scope.submit = function() {
+
+      $http.put('/opportunities/'+$routeParams.oppId, this.opp).success(function(data) {
+        alert('Opportunity Updated');
+        $location.path('/manage');
+      });
+
+    }
+
+    $scope.publish = function() {
+
+      $http.get('/api/government/opportunities/publish/'+$routeParams.oppId).success(function(data) {
+        alert('Opportunity Published');
+        $location.path('/manage');
+      });
+
+    }
 
 });
 
